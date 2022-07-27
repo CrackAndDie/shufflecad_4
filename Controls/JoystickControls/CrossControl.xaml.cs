3871954a721d4a4268a5174a68b3d15141a95b98
Airat <abdrakovairat@gmail.com>
2022-07-27 13:31:25 +0300

    stick and cross controls added

---
+++ Controls/JoystickControls/CrossControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace shufflecad_4.Controls.JoystickControls
+{
+    /// <summary>
+    /// Interaction logic for CrossControl.xaml
+    /// </summary>
+    public partial class CrossControl : UserControl
+    {
+        public CrossControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetValue(float angle)
+        {
+            if (angle == -1)
+            {
+                CrossGradient.GradientOrigin = new Point(0.5, 0.5);
+            }
+            else
+            {
+                angle /= 100;
+                double rad = Math.PI * angle / 180f;
+                double x = Math.Cos(rad);
+                double y = Math.Sin(rad);
+                CrossGradient.GradientOrigin = new Point(x, y);
+            }
+        }
+    }
+}
---
+++ Controls/JoystickControls/StickControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using shufflecad_4.Helpers;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace shufflecad_4.Controls.JoystickControls
+{
+    /// <summary>
+    /// Interaction logic for StickControl.xaml
+    /// </summary>
+    public partial class StickControl : UserControl
+    {
+        private static readonly float[] inArr = { 0, 65535 };
+        private static readonly float[] outArr = { -10, 10 };
+
+        public StickControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetValue(int x, float y)
+        {
+            int xMargin = (int)FuncadHelper.Transfunc(x, inArr.ToList(), outArr.ToList());
+            int yMargin = (int)FuncadHelper.Transfunc(y, inArr.ToList(), outArr.ToList());
+            Stick.Margin = new System.Windows.Thickness(xMargin, yMargin, 0, 0);
+        }
+    }
+}
